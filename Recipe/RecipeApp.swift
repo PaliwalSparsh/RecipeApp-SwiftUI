bea4f64b4abe2aa0d3eeb22680c7349e03a9e92c
Sparsh Paliwal <sparshpailwal123@gmail.com>
2023-02-08 23:58:56 -0500

    Initial Commit

---
+++ Recipe/RecipeApp.swift
@@ -0,0 +1,17 @@
+//
+//  RecipeApp.swift
+//  Recipe
+//
+//  Created by Sparsh Paliwal on 2/8/23.
+//
+
+import SwiftUI
+
+@main
+struct RecipeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
